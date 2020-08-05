621d7112f717c4c3db1c16f3ba395a7140e00b99
absognety <vikasch.1994@gmail.com>
2020-08-05 08:22:58 +0530

    Add __init__.py to treat git_integrate as a package

---
+++ git_integrate/__init__.py
@@ -0,0 +1,5 @@
+from .dbstore import create_connection
+from .driver import initialize_repo
+from .driver import store_issue_attrs
+from .driver import compute_stats_issues
+from .driver import populate_tables
